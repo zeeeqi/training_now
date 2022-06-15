c1782e7138ce59da46a287b618b91079868d1a56
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2022-06-15 09:04:16 +0200

    [DEL] Minutes and seconds from hours

--- bookings/management/commands/generate_hours.py
+++ bookings/management/commands/generate_hours.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         days = {
-            '1':[time(9,0,0,0,), time(10), time(11), time(12), time(13), time(15), time(16), time(17), time(18), time(19), time(20), time(21)],
+            '1':[time(9), time(10), time(11), time(12), time(13), time(15), time(16), time(17), time(18), time(19), time(20), time(21)],
             '2':[time(9), time(10), time(11), time(12), time(13), time(15), time(16), time(17), time(18), time(19), time(20), time(21)],
             '3':[time(9), time(10), time(11), time(12), time(13), time(15), time(16), time(17), time(18), time(19), time(20), time(21)],
             '4':[time(9), time(10), time(11), time(12), time(13), time(15), time(16), time(17), time(18), time(19), time(20), time(21)],
