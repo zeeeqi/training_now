034dd28fc913b71eaf39320d46633a2473574ea3
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2022-06-15 09:14:32 +0200

    [MOD] Refactor urls

--- training_now/urls.py
+++ training_now/urls.py
@@ -25,10 +25,13 @@ urlpatterns = [
     path('', include('users.urls')),
     path('', include('bookings.urls')),
     path('api/', include('api.urls')),
-    path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name="users/password_reset.html",
-        html_email_template_name="emails/reset_password.html",
-        ), name='password_reset'),
+    path('password-reset/', 
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset.html",
+            html_email_template_name="emails/reset_password.html",
+        ),
+        name='password_reset'
+    ),
     path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_sent.html"), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_form.html"), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_done.html"), name='password_reset_complete'),
